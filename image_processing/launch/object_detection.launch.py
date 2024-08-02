0d22d79649d673311e8c40c8dc485fe0bba19b85
thatlittlemoonfromstarwars <andor.siegers@gmail.com>
2024-08-02 10:48:39 -0600

    Fixed launch files

--- image_processing/launch/object_detection.launch.py
+++ image_processing/launch/object_detection.launch.py
@@ -6,14 +6,6 @@ from launch.conditions import IfCondition
 
 def generate_launch_description():
     return LaunchDescription([
-        # Declare parameters
-        DeclareLaunchArgument('prompt', default_value='person.phone', description='Prompt parameter (multiple objects can be detected using a \'.\' as a separator)'),
-        DeclareLaunchArgument('target', default_value='person', description='Target parameter (must be contained in the prompt)'),
-        DeclareLaunchArgument('print_output', default_value='true', description='print_output parameter'),
-        DeclareLaunchArgument('clear_output', default_value='true', description='clear_output parameter'),
-        DeclareLaunchArgument('rgb_img_topic', default_value='zed/zed_node/rgb/image_rect_color', description='topic for rgb image publishing'),
-        DeclareLaunchArgument('depth_img_topic', default_value='zed/zed_node/depth/depth_registered', description='topic for registered depth image publishing'),
-
         # Declare arguments to enable or disable nodes
         DeclareLaunchArgument('run_img_display', default_value='false', description='Run image display node'),
         DeclareLaunchArgument('run_img_save', default_value='false', description='Run image save node'),
@@ -26,12 +18,12 @@ def generate_launch_description():
             name='img_processor_node',
             output='screen',
             parameters=[
-                {'prompt': LaunchConfiguration('prompt')},
-                {'target': LaunchConfiguration('target')},
-                {'print_output': LaunchConfiguration('print_output')},
-                {'clear_output': LaunchConfiguration('clear_output')},
-                {'rgb_img_topic': LaunchConfiguration('rgb_img_topic')},
-                {'depth_img_topic': LaunchConfiguration('depth_img_topic')}
+                {'prompt': 'person.phone'},
+                {'target': 'person'},
+                {'print_output': 'false'},
+                {'clear_output': 'true'},
+                {'rgb_img_topic': 'zed/zed_node/rgb/image_rect_color'},
+                {'depth_img_topic': 'zed/zed_node/depth/depth_registered'}
             ]
         ),
 
--- image_processing/launch/object_detection_lite.launch.py
+++ image_processing/launch/object_detection_lite.launch.py
@@ -5,26 +5,18 @@ from launch_ros.actions import Node
 
 def generate_launch_description():
     return LaunchDescription([
-        # Declare parameters
-        DeclareLaunchArgument('prompt', default_value='person.phone', description='Prompt parameter (multiple objects can be detected using a \'.\' as a separator)'),
-        DeclareLaunchArgument('target', default_value='person', description='Target parameter (must be contained in the prompt)'),
-        DeclareLaunchArgument('print_output', default_value='true', description='print_output parameter'),
-        DeclareLaunchArgument('clear_output', default_value='true', description='clear_output parameter'),
-        DeclareLaunchArgument('rgb_img_topic', default_value='zed/zed_node/rgb/image_rect_color', description='topic for rgb image publishing'),
-        DeclareLaunchArgument('depth_img_topic', default_value='zed/zed_node/depth/depth_registered', description='topic for registered depth image publishing'),
-
         Node(
             package='image_processing',
             executable='image_processor',
             name='img_processor_node',
             output='screen',
             parameters=[
-                {'prompt': LaunchConfiguration('prompt')},
-                {'target': LaunchConfiguration('target')},
-                {'print_output': LaunchConfiguration('print_output')},
-                {'clear_output': LaunchConfiguration('clear_output')},
-                {'rgb_img_topic': LaunchConfiguration('rgb_img_topic')},
-                {'depth_img_topic': LaunchConfiguration('depth_img_topic')}
+                {'prompt': 'person.phone'},
+                {'target': 'person'},
+                {'print_output': 'true'},
+                {'clear_output': 'true'},
+                {'rgb_img_topic': 'zed/zed_node/rgb/image_rect_color'},
+                {'depth_img_topic': 'zed/zed_node/depth/depth_registered'}
             ]
         )
     ])
