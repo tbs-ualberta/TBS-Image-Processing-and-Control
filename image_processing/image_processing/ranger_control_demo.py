311f0ccd25719eb82ac06a56e76d6efd7e897539
Andor Siegers <andor@ualberta.ca>
2024-08-06 16:14:33 -0600

    Updated ranger control node

--- image_processing/image_processing/ranger_control_demo.py
+++ image_processing/image_processing/ranger_control_demo.py
@@ -38,26 +38,34 @@ class RangerController(Node):
         tar_y = target_msg.y
         tar_z = target_msg.z
 
-        # Find angle to target from z-axis coming out of depth camera
-        target_ang = math.atan(tar_x/tar_z) # y/x (in rad)
-        target_dist = math.sqrt(tar_x**2 + tar_z**2) # uses linear distance away from target in the horizontal plane (in meters)
-
-        # Define velocities
-        lin_vel = 0
-        ang_vel = 0
-
-        if target_ang < -self.ANG_TOL/2:
-            ang_vel = self.ANG_VEL
-        elif target_ang > self.ANG_TOL/2:
-            ang_vel = -self.ANG_VEL
-        
-        if target_dist > self.FOLLOW_DIST:
-            lin_vel = self.LIN_VEL
-        
+        # Define robot velocities
         vel = Twist()
-        vel.linear.x = lin_vel
-        vel.angular.z = ang_vel
+        
+        # Check if target was found in image
+        if tar_x == -1 and tar_y == -1 and tar_z == -1:
+            # If target not found in image
+            vel.linear.x = 0
+            vel.angular.z = 0
+        else:
+            # If target found in image
 
+            # Find angle to target from optical axis (perpendicular to sensor)
+            target_ang = math.atan(tar_x/tar_z) # y/x (in rad)
+
+            # Find linear distance from camera to target in the horizontal plane
+            target_dist = math.sqrt(tar_x**2 + tar_z**2) # in meters
+
+            # Check if target is within angle tolerance
+            if target_ang < -self.ANG_TOL/2:    # if target is too far right, rotate ccw
+                vel.linear.x = self.ANG_VEL
+            elif target_ang > self.ANG_TOL/2:   # if target is too far left, rotate cw
+                vel.linear.x = -self.ANG_VEL
+            
+            # Check if target is within following distance
+            if target_dist > self.FOLLOW_DIST:
+                vel.angular.z = self.LIN_VEL
+
+        # Publish velocity data
         self.cmd_vel_pub.publish(vel)
 
     def cleanup(self):
